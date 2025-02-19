408a1d237dec9bab370aa154e97f9595d2f30cb5
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-02-19 12:27:25 -0800

    Update.

--- main.js
+++ main.js
@@ -2,10 +2,52 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Actions taken after the DOM has loaded 
+function doThings(){
 
+  // Hide error message by default 
+  let error = document.getElementById("modal");
+  error.classList.add('hidden')
 
+  // Add event listener for heart clicks 
+  let likes = document.getElementsByClassName("like-glyph");  
+  for (i=0; i< likes.length; i++) {
+    let item = likes[i]
+    item.addEventListener("click", serverCall)
+  }
+  
+  // A function for updating the DOM based on heart clicks 
+  function updateHeart(action){
+    console.log(`inside update heart: ${action.innerText}`)
+    
+    let heart = action.innerText;
 
+    if (heart === EMPTY_HEART){
+      action.classList.add("activated-heart")
+      action.innerText = FULL_HEART;
+    }
+    else if (heart === FULL_HEART){
+      action.classList.remove("activated-heart")
+      action.innerText = EMPTY_HEART;
+    } 
+
+  }
+
+  // A function that calls the 'server' when a heart is clicked
+  function serverCall(event){
+    let action = event.target;
+    mimicServerCall()
+    .then((res)=>res.json)
+    .then(updateHeart(action))
+    .catch(()=>{error.classList.remove('hidden')})
+    .catch(setTimeout(()=>{error.classList.add('hidden')},3000)) 
+    // Show alert [missing]
+  }
+
+}
+
+// Listening for the DOM to load
+document.addEventListener("DOMContentLoaded", doThings);
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
